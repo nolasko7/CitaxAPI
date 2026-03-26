22542503da79037af21c0629524596085c20de47
Valentin <valentin.nolasco123123@gmail.com>
2026-03-26 18:23:33 -0300

    Cambios en los puertos que escuchamos

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Citax API running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Citax API running on 0.0.0.0:${PORT}`);
 });
